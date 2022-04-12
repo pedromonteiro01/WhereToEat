f243f59e2cbda9b22ff497cef22b68e2ffcfb597
Eduardo Fernandes <eduardo@MBP-de-Eduardo.lan>
2022-04-12 16:13:55 +0100

    map working with polylines

---
+++ mapbox_navigation/lib/constants/restaurants.dart
@@ -0,0 +1,23 @@
+List<Map> restaurants = [
+  {
+    'id': '0',
+    'name': 'Ramona',
+    'items': 'Francesinha, Hamburguer, Sandes',
+    'image': 'https://scontent.fopo4-2.fna.fbcdn.net/v/t1.6435-9/132858036_225383902444943_5548837240534816498_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEgbxjvOMF62E9xIfTJOlrfZ2v656rb0zhna_rnqtvTOCiXh3_rEpZJQXttfKurORZH3FOFQ-pBhfXgBwgmI072&_nc_ohc=S1XdkgqdDeMAX_2HOAZ&_nc_ht=scontent.fopo4-2.fna&oh=00_AT9dWi6UZZhA4OjCg-XJp58AxSzDAie8yIJWuqcotUe-gQ&oe=627CE4E3',
+    'coordinates': {
+      'latitude': '40.64823721419626',
+      'longitude': '-8.647542077590103',
+    },
+  },
+  {
+    'id': '0',
+    'name': 'Convivio',
+    'items': 'Hamburguer, Sandes, Francesinha, Snacks',
+    'image':
+        'https://indiaeducationdiary.in/wp-content/uploads/2020/10/IMG-20201024-WA0014.jpg',
+    'coordinates': {
+      'latitude': '40.63418681813457',
+      'longitude': '-8.64836730215535',
+    },
+  },
+];
